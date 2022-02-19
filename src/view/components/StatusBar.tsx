bcd86298cc96b900eb438fa4ab4e856b08e8755f
Cheng Ziqiu <jxlpzqc@live.com>
2022-02-19 16:54:17 +0800

    Complete scale bar.

--- src/view/components/StatusBar.tsx
+++ src/view/components/StatusBar.tsx
@@ -13,29 +13,78 @@ interface IStatusBarProps {
   onzoom: (zoom: number) => void;
 }
 
+/**
+ * Returns zoom value
+ * @param v value ranges from -100 to 100
+ */
+function sliderValueToZoom(v: number): number {
+  if (v >= 0) {
+    return v / 100 * (4 - 1) + 1;
+  }
+  else {
+    return 1 + v / 100 * (1 - 0.1);
+  }
+
+}
+
+/**
+ * Returns slider value
+ * @param v value ranges from 0 to 4
+ */
+function zoomToSliderValue(v: number): number {
+  if (v >= 1) {
+    return (v - 1) / (4 - 1) * 100;
+  }
+  else {
+    return (v - 1) / (1 - 0.1) * 100;
+  }
+}
+
 const StatusBar: React.FunctionComponent<IStatusBarProps> = ({
   desc, progress, view = 'default', zoom, onzoom
 }) => {
 
   const theme = getTheme();
-  const sliderStyle: IStyleFunctionOrObject<ISliderStyleProps, ISliderStyles> = {
+  const sliderStyle: Partial<ISliderStyles> = {
     thumb: {
-      background: theme.palette.neutralDark,
+      background: theme.palette.neutralPrimary,
       width: '8px',
       height: '16px',
+      border: 0,
       borderRadius: 0
     },
     slideBox: {
       height: '16px'
-    }
+    },
+    zeroTick: {
+      background: theme.palette.neutralPrimary,
+      height: 8,
+      transform: "translateY(-2px)"
+    },
+    lineContainer: {
+      background: theme.palette.neutralDark
+    },
   }
 
   const btnStyle: IButtonStyles = {
     root: {
       height: 20,
+      width: 50,
       color: theme.palette.black
     }
   };
+
+  const zoomPlus = () => {
+    let z = Math.floor(zoom * 10 + 1) / 10;
+    if (z > 4) z = 4;
+    onzoom(z);
+  }
+
+  const zoomMinus = () => {
+    let z = Math.floor(zoom * 10 - 1) / 10;
+    if (z < 0.1) z = 0.1;
+    onzoom(z);
+  }
 
   return (
     <Stack styles={{
@@ -61,20 +110,23 @@ const StatusBar: React.FunctionComponent<IStatusBarProps> = ({
           <IconButton styles={btnStyle} size={10} iconProps={{
             iconName: 'Remove'
           }} title="-" ariaLabel="-" disabled={false}
-            onClick={() => { onzoom(zoom - 0.1) }} />
+            onClick={zoomMinus} />
 
           <Stack.Item grow align='center'>
-            <Slider value={zoom} styles={sliderStyle} min={0.2} max={5} showValue={false} />
+            <Slider
+              value={zoomToSliderValue(zoom)}
+              onChange={(value) => { onzoom(sliderValueToZoom(value)) }}
+              styles={sliderStyle} min={-100} max={100} showValue={false} originFromZero />
           </Stack.Item>
           <IconButton styles={btnStyle} size={10} iconProps={{
             iconName: 'Add'
           }} title="+" ariaLabel="+" disabled={false}
-            onClick={() => { onzoom(zoom + 0.1) }} />
+            onClick={zoomPlus} />
         </Stack>
       </Stack.Item>
 
       <Stack.Item>
-        <ActionButton styles={btnStyle} text={(zoom * 100).toFixed(0) + "%"} height={32} disabled={false} />
+        <ActionButton onClick={() => { onzoom(1) }} styles={btnStyle} text={(zoom * 100).toFixed(0) + "%"} height={32} disabled={false} />
       </Stack.Item>
     </Stack>
 
